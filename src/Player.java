28387b9a73920abbbc7ce2e44f8fad5616aa49e5
uxeyali <uzma.aliza@gmail.com>
2018-09-23 21:20:07 -0500

    Game logic complete

--- src/Player.java
+++ src/Player.java
@@ -5,8 +5,13 @@ public class Player {
 	//constructor
 		public Player() {
 			this.hand = new NPSOrderedArrayList<Card>();
+			this.score = 0;
 		}
 		
-		
+	public Card play() {
+		Card cardPlayed = this.hand.get(0);
+		hand.remove(0);
+		return cardPlayed;
+	}
 		
 }
