0e95f35390b75c56cec8a2003d54b3be40bb4373
nekrosword <33295352+nekrosword@users.noreply.github.com>
2018-09-27 15:08:30 -0500

    Interface upadte

--- src/Interface.java
+++ src/Interface.java
@@ -1,8 +1,84 @@
 import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.layout.*;
 import static org.eclipse.swt.events.SelectionListener.*;
 public class Interface {
+	
+	public static void Welcome() {
+		/*
+		 * splash screen
+		 */
+		Display Welcome = new Display();
+		Shell Start = new Shell(Welcome);
+		Start.setLayout(new GridLayout());
+		Image image = new Image(Welcome, Interface.class.getResourceAsStream("back_cards-07.png"));
+		final int width = image.getBounds().width;
+		final int height = image.getBounds().height;
+		final Image scaled050 = new Image(Welcome,
+		        image.getImageData().scaledTo((int)(width*0.5),(int)(height*0.5)));
+		Button Local = new Button(Start, SWT.PUSH);
+		Button Online = new Button(Start, SWT.PUSH);
+		Start.setBackgroundImage(scaled050);
+		Local.setText("Local Game");
+		Online.setText("Online Game");
+		
+		Local.addListener(SWT.Selection, e -> Player1());
+		Player p1Player = new Player();
+		//p1Player.hand.get(1).suit;
+		//p1Player.hand.get(1).value;
+		Start.setSize(1640, 800);
+		Local.pack();
+		Online.pack();
+		Start.open();
+		while (!Start.isDisposed ()) {
+			if (!Welcome.readAndDispatch ()) Welcome.sleep ();
+		}
+		Welcome.dispose ();
+
+	}
+	private static void Player1() {
+		/*
+		 * This method defines the use
+		 * of Player 1
+		 * The window will have 4
+		 * main areas. 
+		 * Player score,
+		 * who's turn it is
+		 * Cards being played
+		 * Cards to choose
+		 */
+		Display player = new Display();
+		Shell Player1 = new Shell(player);
+		Label score = new Label(Player1, SWT.BORDER);
+		Label turn = new Label(Player1, SWT.BORDER);
+		Button button = new Button(Player1, SWT.PUSH);
+		
+		button.setLocation(100, 100);
+		button.setSize(300, 300);
+		button.setText("current");
+		Player1.setSize(700, 700);
+		score.setLocation(0, 0);
+		score.setText("Your Score:");
+		score.setSize(80, 50);
+		turn.setLocation(300, 0);
+		turn.setSize(200, 100);
+		turn.setText("Who's Turn");
+		Image pic = new Image(player, Interface.class.getResourceAsStream("PNG/2C.png"));
+		final int width = pic.getBounds().width;
+		final int height = pic.getBounds().height;
+		final Image scaled050 = new Image(player,
+		        pic.getImageData().scaledTo((int)(width*0.2),(int)(height*0.2)));
+		button.addListener(SWT.MouseHover, e-> System.out.println("wow"));
+		button.setImage(scaled050);
+		Player1.open();
+		while (!Player1.isDisposed()) {
+			if (!player.readAndDispatch()) player.sleep();
+		}
+		player.dispose();
+		
+		
+	}
 
 	public static void Display() {
 	Display display = new Display();
@@ -38,6 +114,6 @@ public class Interface {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Display();
+		Player1();
 	}
 }
